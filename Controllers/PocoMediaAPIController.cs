c0776eeac0545f28bfc7fb42ddfab81036e9c751
Thangamani Eraniyan <talk2gold@live.com>
2023-11-15 19:26:12 -0800

    Add project files.

---
+++ Controllers/PocoMediaAPIController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MediaAPI.Controllers
+{
+    //Type of Api call
+    //GET   -- reading /getting record from api
+    //POST  -- Create Records
+    //PUT   -- Update Record
+    //DELETE --Delete Record
+    //PATCH  -- Update Partial record  -- only selected properties
+	public class PocoMediaAPIController : ControllerBase
+	{
+
+        public void getMedia()
+        { 
+        }
+    }
+}
